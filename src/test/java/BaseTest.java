98dd8e37df9910ed77e6bbf8e4398c2462db322b
Mohamed Taqi <126124586+MedTaqi@users.noreply.github.com>
2023-11-23 06:51:26 -0500

    Update BaseTest.java

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,12 +1,21 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
 
 public class BaseTest {
-
+webdriver driver;
     @BeforeSuite
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
+    }
+    @BeforeMethod({"BaseURL"})
+    public static void launchBrowser(String BaseURL)
+        driver = new FirefoxDriver();
+
+
+
     }
 
 }
