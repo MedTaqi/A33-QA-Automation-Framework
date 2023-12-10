e59f1ec09c38388f10ebe0506518729b810f4a94
Mohamed Taqi <126124586+MedTaqi@users.noreply.github.com>
2023-12-10 18:55:28 -0500

    cc

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,12 +1,42 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+
+import java.time.Duration;
 
 
 public class BaseTest {
-
+public WebDriver driver;
+public String url=null;
+public Actions actions;
+public WebDriverWait wait;
     @BeforeSuite
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
+    }
+    @BeforeMethod
+    @Parameters({"BaseURL"})
+    public void launchBrowser( String BaseURL){
+        ChromeOptions opts = new ChromeOptions();
+        opts.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(opts);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+        wait=new WebDriverWait(driver,Duration.ofSeconds(10));
+        actions = new Actions(driver);
+        url=BaseURL;
+        driver.get(url);
+
+
+
+
     }
 
 }
